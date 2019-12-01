58dea28f5809059931fbe848de2dce5c58181ef7
ziqidonglai88 <746769845@qq.com>
2019-12-01 13:53:03 +0800

    修改文件

--- uboot-imx-rel_imx_4.1.15_2.1.0_ga/mx6ull_alientek_emmc.sh
+++ uboot-imx-rel_imx_4.1.15_2.1.0_ga/mx6ull_alientek_emmc.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 make ARCH=arm CROSS_COMPILE=/opt/tool/gcc-linaro-4.9.4-2017/bin/arm-linux-gnueabihf- distclean
 make ARCH=arm CROSS_COMPILE=/opt/tool/gcc-linaro-4.9.4-2017/bin/arm-linux-gnueabihf mx6ull_alientek_emmc_defconfig
 make V=1 ARCH=arm CROSS_COMPILE=/opt/tool/gcc-linaro-4.9.4-2017/bin/arm-linux-gnueabihf- -j12
